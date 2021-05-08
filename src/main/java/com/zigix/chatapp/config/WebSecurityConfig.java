3a3f4dce245f05825aa8d54f0781da5da823ef38
Zigix <olekwszk7@gmail.com>
2021-05-08 12:45:27 +0200

    Added starting page

--- src/main/java/com/zigix/chatapp/config/WebSecurityConfig.java
+++ src/main/java/com/zigix/chatapp/config/WebSecurityConfig.java
@@ -33,13 +33,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/")
+                .antMatchers("/", "/css/**", "/js/**", "/img/**").permitAll()
+                .anyRequest()
                 .authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .permitAll()
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/public-chat")
+                .permitAll();
     }
 
     @Bean
